7ea5abbc3cada8b09be969d194f0b1afb9a692ae
Christian Groothoff <ruffo324@gmail.com>
2020-10-25 03:11:23 +0100

    Untested steps for debugging

---
+++ StripeTastic/cmake-build-doit-esp-prog-debug/CMakeFiles/Z_DUMMY_TARGET.dir/cmake_clean.cmake
@@ -0,0 +1,36 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/doit-esp-prog-debug/AsyncTCP/src/AsyncTCP.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/doit-esp-prog-debug/ESPAsyncTCP/src/AsyncPrinter.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/doit-esp-prog-debug/ESPAsyncTCP/src/ESPAsyncTCP.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/doit-esp-prog-debug/ESPAsyncTCP/src/ESPAsyncTCPbuffer.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/doit-esp-prog-debug/ESPAsyncTCP/src/SyncClient.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/doit-esp-prog-debug/ESPAsyncTCP/src/tcp_axtls.c.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/doit-esp-prog-debug/ESP_Async_WebServer/src/AsyncEventSource.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/doit-esp-prog-debug/ESP_Async_WebServer/src/AsyncWebSocket.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/doit-esp-prog-debug/ESP_Async_WebServer/src/SPIFFSEditor.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/doit-esp-prog-debug/ESP_Async_WebServer/src/WebAuthentication.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/doit-esp-prog-debug/ESP_Async_WebServer/src/WebHandlers.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/doit-esp-prog-debug/ESP_Async_WebServer/src/WebRequest.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/doit-esp-prog-debug/ESP_Async_WebServer/src/WebResponses.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/doit-esp-prog-debug/ESP_Async_WebServer/src/WebServer.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/doit-esp-prog-debug/arduinoFFT/src/arduinoFFT.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/lib/Configuration/Configuration.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/lib/Services/FileService.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/lib/Services/Logger.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/lib/Services/LoopService.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/lib/Services/WebService.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/lib/Services/WifiService.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/lib/StripeBridge/src/Constants/Colors.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/lib/StripeBridge/src/StripeBridge.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/lib/StripeBridge/src/StripeInstance.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/src/main.cpp.obj"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/src/plannedStructure.cpp.obj"
+  "Z_DUMMY_TARGET"
+  "Z_DUMMY_TARGET.manifest"
+  "Z_DUMMY_TARGET.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/Z_DUMMY_TARGET.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
