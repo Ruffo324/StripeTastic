ea648830add966d38707002436b67e4b3be78ef8
Christian Groothoff <ruffo324@gmail.com>
2020-10-27 22:32:16 +0100

    Removed all tracked but ignored files for clean structure

--- StripeTastic/data_src/Utils/TypeChecker.ts
+++
@@ -1,9 +0,0 @@
-export module TypeChecker {
-    /**
-     * Checks if any of the arguments is undefined.
-     * @param objects Objects to check.
-     */
-    export function isAnyUndefined(...objects: any[]): boolean {
-        return objects.some((obj) => typeof obj === "undefined");
-    }
-}
--- StripeTastic/data_src/app.ts
+++
@@ -1,8 +0,0 @@
-import { NavigationModule } from "./Modules/NavigationModule";
-import { ServerEventListener } from "./Modules/ServerEventListener";
-
-// Load and Bind all required modules.
-$(() => {
-    NavigationModule.Bind();
-    ServerEventListener.Listen();
-});
